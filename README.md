55dffb1a96aafdf96cd6b0c0cbbb83b89c9c8979
umaumax <umauma7083@gmail.com>
2018-10-15 22:29:08 +0900

    Add files

---
+++ README.md
@@ -0,0 +1,33 @@
+# ros_link_other_shared_library_tutorials
+
+```
+find_package(catkin REQUIRED COMPONENTS
+  roscpp
+  std_msgs
+  ros_shared_library_tutorials
+)
+```
+
+`find_package`でパッケージを指定することで，そのパッケージで適切に，`LIBRARIES`と`INCLUDE_DIRS`が指定されてれば，下記のように明示的に指定する必要性はない
+
+```
+target_link_libraries(${PROJECT_NAME}_node
+# ${ros_shared_library_tutorials_LIBRARIES}
+  ${catkin_LIBRARIES}
+)
+
+
+include_directories(
+# include
+# ${ros_shared_library_tutorials_INCLUDE_DIRS}
+  ${catkin_INCLUDE_DIRS}
+)
+```
+
+----
+
+## FYI
+init gen command
+```
+catkin_create_pkg ros_link_other_shared_library_tutorials std_msgs roscpp
+```
---
+++ src/ros_link_other_shared_library_tutorials_node.cpp
@@ -0,0 +1,34 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#include <iostream>
+#include <sstream>
+
+#include "ros_shared_library_tutorials/add.hpp"
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "talker");
+  ros::NodeHandle n;
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
+  ros::Rate loop_rate(10);
+
+  std::cout << "add result:" << add(1, 2) << std::endl;
+
+  int count = 0;
+  while (ros::ok()) {
+    std_msgs::String msg;
+
+    std::stringstream ss;
+    ss << "hello world " << count;
+    msg.data = ss.str();
+
+    ROS_INFO("%s", msg.data.c_str());
+
+    chatter_pub.publish(msg);
+    ros::spinOnce();
+    loop_rate.sleep();
+    ++count;
+  }
+
+  return 0;
+}
